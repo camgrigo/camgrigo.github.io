7f9356e9cbbaf5f8031aa8cc0edcfe648ef0e276
Cam <camgrigoriadis@gmail.com>
2019-07-15 19:56:52 -0700

    Fix some layout issues, add to site

---
+++ scripts/content-loading.js
@@ -0,0 +1,90 @@
+// profiles.js
+// (c) 2019 Cameron Grigoriadis
+
+var profileSectionId = 'profile-link-section'
+var workSectionId = 'work-section-content'
+
+function loadProfiles() {
+  var profiles = [
+    [
+      "gitHub-profile-link",
+      "https://github.com/camgrigo",
+      ["resources/icons/profiles/GitHub-Mark/PNG/GitHub-Mark-120px-plus.png", "View my Github Profile"]
+    ],
+    [
+      "linkedIn-profile-link",
+      "https://www.linkedin.com/in/cameron-grigoriadis-4a4119184",
+      ["resources/icons/profiles/LinkedIn-Logos/LI-In-Bug.png", "View my LinkedIn® Profile"]
+    ],
+    [
+      "instagram-profile-link",
+      "https://www.instagram.com/servicedayapp/",
+      ["resources/icons/profiles/GlyphLogo_May2016_Onlinev2/glyph-logo_May2016.png", "Visit the Service Day Instagram Profile"]
+    ],
+    [
+      "twitter-profile-link",
+      "https://twitter.com/camgrigo",
+      ["resources/icons/profiles/Twitter Social Icons/Twitter Social Icons/Twitter_SocialIcon_Circle/Twitter_Social_Icon_Circle_Color.png", "View my Twitter Profile"]
+    ]
+  ]
+
+  document.getElementById(profileSectionId).innerHTML = profiles
+  .map(function(profile) {
+    return '<div id="' + profile[0] + '" class="profile-link-sm">' +
+    '<a href="' + profile[1] + '" target="_blank">' +
+    '<img src="' + profile[2][0] + '" alt="' + profile[2][1] + '">' +
+    '</a>' +
+    '</div>'
+  })
+  .join('\n')
+}
+
+function loadWorkSection() {
+  var content = [
+    [
+      '<img src="resources/images/service-day-icon.png" alt="Service Day App Icon" style="max-width: 10rem;">',
+      'Service Day',
+      'An app for the ministry.',
+      '<a href="https://apps.apple.com/us/app/service-day/id1204528942?mt=8" style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2019-03-06&kind=iossoftware&bubble=ios_apps) no-repeat;width:135px;height:40px;" target="_blank"></a>'
+    ],
+    [
+      '<img src="" alt="Happy Yogurt" style="max-width: 10rem;">',
+      'Happy Yogurt',
+      'Employee, 2017 – 2018',
+      ''
+    ],
+    [
+      '',
+      '3-Screens Studio',
+      'Intern (Spring 2016)',
+      ''
+    ],
+    [
+      '<img src="" alt="SWINGy Box Screenshot">',
+      'SWINGy Box',
+      'A team project for my Java Graphic User Interface class final.',
+      ''
+    ]
+  ]
+
+  document.getElementById(workSectionId).innerHTML = content
+  .map(function(item) {
+    var hrWidth = 40
+
+    return '<div class="row">' +
+    '<div class="col-md-1"></div>' +
+    '<div class="col-md-4" align="right">' + item[0] + '</div>' + // Image
+    '<div class="col-md-1"></div>' +
+    '<div class="col-md-5">' +
+    '<div class="header">' +
+    '<h3>' + item[1] + '</h3>' + // Title
+    '<h6>' + item[2] + '</h6>' + // Tagline
+    '</div>' +
+    item[3] + // Call to action
+    '</div>' +
+    '</div>' +
+    '<div class="col-md-1"></div>' +
+    '<hr class="col-12" style="width: ' + hrWidth + '%">'
+  })
+  .join('\n')
+}
